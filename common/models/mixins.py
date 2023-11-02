041447b56bbed1a6f9c6dc613693541b3268eac2
secdet <turgunovjamshid32@gmail.com>
2023-11-02 17:08:15 +0500

    mixin,views: delete funtion

--- common/models/mixins.py
+++ common/models/mixins.py
@@ -1,16 +1,5 @@
-from rest_framework import status, viewsets
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse, HttpResponseBadRequest
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-from django.views import View
-from common.models.mixins import Product, ShoppingCart, Category
-from .serializer import CategorySerializer, ProductSerializer
-
+from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -27,25 +16,3 @@ class BaseDictModelMixin(models.Model):
 
     def __str__(self):
         return f'{self.code} ({self.name})'
-
-class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    price = models.FloatField()
-    color = models.CharField(max_length=50)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class Category(MPTTModel):
-    parent = TreeForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-class ShoppingCart(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    count = models.PositiveIntegerField(
-        validators=[MinValueValidator(1)], default=1
-    )
-    creates_at = models.DateTimeField(auto_now_add=True)
-
--- common/views.py
+++ common/views.py
@@ -1,36 +1,3 @@
 from django.shortcuts import render
 
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse, HttpResponseBadRequest
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
-from django.views import View
-from .models import ShoppingCart
-class AddToShoppingCartView(APIView):
-    @csrf_exempt
-    @require_POST
-    def post(self, request):
-        if not request.user.is_authenticated:
-            return HttpResponseBadRequest("User is not authenticated")
-
-        product_id = request.POST.get("product_id")
-        count = request.POST.get("count", 1)
-
-        if not product_id:
-            return HttpResponseBadRequest("Product ID is required")
-
-        product = get_object_or_404(Product, id=product_id)
-
-        try:
-            count = int(count)
-            if count < 1:
-                return HttpResponseBadRequest("Count must be greater than 0")
-        except ValueError:
-            return HttpResponseBadRequest("Invalid count")
-
-        shopping_cart, created = ShoppingCart.objects.get_or_create(user=request.user, product=product)
-        if not created:
-            shopping_cart.count += count
-            shopping_cart.save()
-
-        return JsonResponse({"message": "Product added to the shopping cart"})
+# Create your views here.
